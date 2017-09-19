e3927b0c76c6839ec76539a56f03c8c4ea2094f8
George Kaimakis <geomatus@mac.com>
2017-09-19 21:13:40 +0100

    Adds '#pragma: no cover' to supress coverage testing at that point:

--- Ch_02/app/calculate.py
+++ Ch_02/app/calculate.py
@@ -18,7 +18,7 @@ class Calculate(object):
             raise TypeError('Invalid type: {} and {}'.format(type(x), type(y)))
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  #pragma: no cover
     calc = Calculate()
     result = calc.add(2, 2)
     print(result)
